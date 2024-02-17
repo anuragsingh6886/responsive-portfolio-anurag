2d48949e6316d7fd401c4112345e23b5a44a8ac0
Anurag Singh <83186717+anuragsingh6886@users.noreply.github.com>
2024-02-17 15:04:36 +0530

    header-changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,47 +2,52 @@ import React from "react";
 import emailLogo from "../assets/images/icons/email.svg";
 import hamburgerMenueMobile from "../assets/images/icons/hamburger-menu.svg";
 export function Navbar(props) {
-
-  const Mailto = ({ email, subject = '', body = '', children }) => {
-    let params = subject || body ? '?' : '';
+  const Mailto = ({ email, subject = "", body = "", children }) => {
+    let params = subject || body ? "?" : "";
     if (subject) params += `subject=${encodeURIComponent(subject)}`;
-    if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+    if (body) params += `${subject ? "&" : ""}body=${encodeURIComponent(body)}`;
 
     return <a href={`mailto:${email}${params}`}>{children}</a>;
   };
 
   return (
-    <section className="container d-flex py-3" id="navbar-head">
-      <div className="header-left d-flex">
-        <Mailto email={props.email} subject="Subject Here" body="Hi, Anurag...">
-          <img src={emailLogo} alt="" />
-          <span>{props.email}</span>
-        </Mailto>
-      </div>
-      <div className="header-right d-none d-md-flex">
-        <div className="about-nav">
-          <a href="#about">
-            <span>About</span>
-          </a>
+    <section className="d-flex py-3" id="navbar-head">
+      <div className="container d-flex navbar-wrap">
+        <div className="header-left d-flex">
+          <Mailto
+            email={props.email}
+            subject="Subject Here"
+            body="Hi, Anurag..."
+          >
+            <img src={emailLogo} alt="" />
+            <span>{props.email}</span>
+          </Mailto>
         </div>
-        <div className="service-nav">
-          <a href="#services">
-            <span>Services</span>
-          </a>
+        <div className="header-right d-none d-md-flex">
+          <div className="about-nav">
+            <a href="#about">
+              <span>About</span>
+            </a>
+          </div>
+          <div className="service-nav">
+            <a href="#services">
+              <span>Services</span>
+            </a>
+          </div>
+          <div className="portfolio-nav">
+            <a href="#portfolio-section">
+              <span>Portfolio</span>
+            </a>
+          </div>
+          <div className="contact-nav">
+            <a href="#footer-head">
+              <span>Contact</span>
+            </a>
+          </div>
         </div>
-        <div className="portfolio-nav">
-          <a href="#portfolio-section">
-            <span>Portfolio</span>
-          </a>
+        <div className="header-right-mobile d-md-none">
+          <img src={hamburgerMenueMobile} alt="" />
         </div>
-        <div className="contact-nav">
-          <a href="#footer-head">
-            <span>Contact</span>
-          </a>
-        </div>
-      </div>
-      <div className="header-right-mobile d-md-none">
-        <img src={hamburgerMenueMobile} alt="" />
       </div>
     </section>
   );
